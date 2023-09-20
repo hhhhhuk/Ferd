3891ad1084196bc9c02baef2b190f7329707f975
Foogg5776 <vcoj05508@gmail.com>
2023-09-21 07:57:52 +1000

    fff

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
+let video = document.querySelector("#Video2")
+
 document.getElementById('open-modal-img').addEventListener("click", function() {
     document.getElementById("mymodal").classList.add("open")
 })
 
 document.getElementById('close-my-modal-btn').addEventListener("click", function() {
     document.getElementById("mymodal").classList.remove("open")
+    video.pause()
 })
 
