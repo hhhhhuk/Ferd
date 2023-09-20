9cebfba02230f2be4e0857bbc50e95bef5d6f7a2
Foogg5776 <vcoj05508@gmail.com>
2023-09-21 00:55:54 +1000

    fff

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,8 @@
-let cimg = document.querySelector('.content__img')
-let modal = document.querySelector('.modal')
-let video = document.querySelector('.main__video')
-function stopVideo(){
-    video.pause();
-    video.currentTime = 0;
-}
+document.getElementById('open-modal-btn').addEventListener("click", function() {
+    document.getElementById("mymodal").classList.add("open")
+})
 
-modal.style.cssText = `
-    display: flex;
-    visibility: hidden;
-    opacity: 0;
-    transition: opacity 0.5s ease-in-out;
-    `;
+document.getElementById('close-my-modal-btn').addEventListener("click", function() {
+    document.getElementById("mymodal").classList.remove("open")
+})
 
-cimg.onclick = () => {
-    modal.style.visibility = 'visible';
-    modal.style.opacity = '1';
-};
-    
-if (TouchEvent === cimg.onclick) {
-    modal.style.visibility = 'visible';
-    modal.style.opacity = '1';
-}
-
-const CloseModal = event => {
-    const target = event.target;
-
-    if (target === modal) {
-        modal.style.visibility = 'hidden';
-        modal.style.opacity = '0';
-        video.pause()
-    }
-};
-
-modal.addEventListener('click', CloseModal);
-modal.addEventListener('touch', CloseModal);
